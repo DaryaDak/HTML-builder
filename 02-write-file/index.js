3144238ee480c634118a598d2ee59bf46ce55ae5
Admin <admin@iMac-Admin.local>
2021-11-07 15:46:11 +0300

    fix: fix solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,22 +5,21 @@ const { stdin, stdout } = process;
 
 const stream = fs.createWriteStream(path.join(__dirname,"textfile.txt"),"UTF-8", () => {
     if(error) throw error;
-    // console.log(data);
 });
-stream.write('Введите текст:\n');
-stdout.write('Введите текст:\n');
+stream.write('Enter text:\n');
+stdout.write('Enter text:\n');
 stdin.on('data', data => {
-    if (data === 'exit\n') { 
-       return process.exit();
+    if (data == 'exit\n') {
+        console.log("Good luck!");
+        process.exit();
     } else {
         stream.write(data);
     }
 });
 
 process.on('SIGINT', () => {
-    console.log('\nУдачного дня!');
-process.exit();
+    console.log('\nBye! Have a good day!');
+    process.exit();
 });
-
 
 
