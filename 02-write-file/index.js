75af36e41256fce1e300a3cfee1f082eea9d1caf
Admin <admin@iMac-Admin.local>
2022-05-16 11:30:07 +0300

    feat: add task two

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,21 +3,22 @@ const path = require('path');
 const process = require('process');
 const { stdin, stdout } = process;
 
-const stream = fs.createWriteStream(path.join(__dirname,"textfile.txt"),"UTF-8", () => {
-    if(error) throw error;
+const stream = fs.createWriteStream(path.join(__dirname,'textfile.txt'),'UTF-8', () => {
+  // eslint-disable-next-line no-undef
+  if(error) throw error;
 });
 stream.write('Enter text:\n');
 stdout.write('Enter text:\n');
 stdin.on('data', data => {
-    if (data.includes('exit')) {
-        console.log("Good luck!");
-        process.exit();
-    } else {
-        stream.write(data);
-    }
+  if (data.includes('exit')) {
+    console.log('Good luck!');
+    process.exit();
+  } else {
+    stream.write(data);
+  }
 });
 
 process.on('SIGINT', () => {
-    console.log('\nBye! Have a good day!');
-    process.exit();
+  console.log('\nBye! Have a good day!');
+  process.exit();
 });
