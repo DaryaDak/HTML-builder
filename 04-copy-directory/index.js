7baa8f295461f8d0b9d0bf41ed4c194f5d380732
Admin <admin@iMac-Admin.local>
2022-05-19 12:37:13 +0300

    feat: add task four

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyfiles() {
+  await fs.promises.rm(path.join(__dirname,'files-copy'), { recursive: true, force: true});
+  await fs.promises.mkdir(path.join(__dirname,'files-copy'), { recursive: true }, (err) => {
+    if(err) throw err;
+  });
+  await fs.readdir(path.join(__dirname,'files'), (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+      fs.copyFile((path.join(__dirname,'files', file)), (path.join(__dirname,'files-copy', file)), (err) => {
+        if(err) throw err;
+      });
+    });
+  });
+}
+copyfiles();
